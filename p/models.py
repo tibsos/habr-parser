710a49500db267dd831dfbe1d316beac6a7633c8
tibsos <kosyakovsn@gmail.com>
2025-01-21 14:06:46 +0300

    xx

--- p/models.py
+++ p/models.py
@@ -1,17 +1,12 @@
 from django.db import models as m
-
 from uuid import uuid4 as u4
 
 class Order(m.Model):
-
-    uid = m.UUIDField(default = u4, unique = True)
-
-    title = m.TextField()
-    price = m.PositiveSmallIntegerField()
-
-    created_at = m.DateTimeField(auto_now_add = True)
-    updated_at = m.DateTimeField(auto_now = True)
+    uid = m.UUIDField(default=u4, unique=True)
+    link = m.URLField(unique=True)  # Ensures no duplicate links
+    title = m.CharField(max_length=255)  # Title of the task
+    created_at = m.DateTimeField(auto_now_add=True)
+    updated_at = m.DateTimeField(auto_now=True)
 
     def __str__(self):
-
-        return f"{self.price}₽ | {self.title}"
+        return f"{self.title} | {self.link}"
